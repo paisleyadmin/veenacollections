samynu/nopCommerce

#489 Added new activity types

src/Presentation/Nop.Web/Administration/Controllers/PluginController.cs
@@ -18,6 +18,7 @@ using Nop.Services.Cms;
 using Nop.Services.Common;
 using Nop.Services.Configuration;
 using Nop.Services.Localization;
+using Nop.Services.Logging;
 using Nop.Services.Payments;
 using Nop.Services.Security;
 using Nop.Services.Shipping;
@@ -46,9 +47,10 @@ namespace Nop.Admin.Controllers
         private readonly TaxSettings _taxSettings;
         private readonly ExternalAuthenticationSettings _externalAuthenticationSettings;
         private readonly WidgetSettings _widgetSettings;
-	    #endregion
+        private readonly ICustomerActivityService _customerActivityService;
+        #endregion
 
-		#region Constructors
+        #region Constructors
 
         public PluginController(IPluginFinder pluginFinder,
             IOfficialFeedManager officialFeedManager,
@@ -62,8 +64,9 @@ namespace Nop.Admin.Controllers
             ShippingSettings shippingSettings,
             TaxSettings taxSettings, 
             ExternalAuthenticationSettings externalAuthenticationSettings, 
-            WidgetSettings widgetSettings)
-		{
+            WidgetSettings widgetSettings,
+            ICustomerActivityService customerActivityService)
+        {
             this._pluginFinder = pluginFinder;
             this._officialFeedManager = officialFeedManager;
             this._localizationService = localizationService;
@@ -77,7 +80,8 @@ namespace Nop.Admin.Controllers
             this._taxSettings = taxSettings;
             this._externalAuthenticationSettings = externalAuthenticationSettings;
             this._widgetSettings = widgetSettings;
-		}
+            this._customerActivityService = customerActivityService;
+        }
 
 		#endregion 
 
@@ -300,6 +304,10 @@ namespace Nop.Admin.Controllers
 
                 //install plugin
                 pluginDescriptor.Instance().Install();
+
+                //activity log
+                _customerActivityService.InsertActivity("AddNewPlugin", _localizationService.GetResource("ActivityLog.AddNewPlugin"), pluginDescriptor.FriendlyName);
+
                 SuccessNotification(_localizationService.GetResource("Admin.Configuration.Plugins.Installed"));
 
                 //restart application
@@ -339,6 +347,10 @@ namespace Nop.Admin.Controllers
 
                 //uninstall plugin
                 pluginDescriptor.Instance().Uninstall();
+
+                //activity log
+                _customerActivityService.InsertActivity("DeletePlugin", _localizationService.GetResource("ActivityLog.DeletePlugin"), pluginDescriptor.FriendlyName);
+
                 SuccessNotification(_localizationService.GetResource("Admin.Configuration.Plugins.Uninstalled"));
 
                 //restart application
@@ -564,6 +576,9 @@ namespace Nop.Admin.Controllers
                             }
                         }
                     }
+
+                    //activity log
+                    _customerActivityService.InsertActivity("EditPlugin", _localizationService.GetResource("ActivityLog.EditPlugin"), pluginDescriptor.FriendlyName);
                 }
 
                 ViewBag.RefreshPage = true;

src/Presentation/Nop.Web/Administration/Controllers/ProductReviewController.cs
@@ -9,6 +9,7 @@ using Nop.Services.Catalog;
 using Nop.Services.Events;
 using Nop.Services.Helpers;
 using Nop.Services.Localization;
+using Nop.Services.Logging;
 using Nop.Services.Security;
 using Nop.Services.Stores;
 using Nop.Web.Framework.Controllers;
@@ -26,6 +27,7 @@ namespace Nop.Admin.Controllers
         private readonly IPermissionService _permissionService;
         private readonly IEventPublisher _eventPublisher;
         private readonly IStoreService _storeService;
+        private readonly ICustomerActivityService _customerActivityService;
 
         #endregion Fields
 
@@ -36,7 +38,8 @@ namespace Nop.Admin.Controllers
             ILocalizationService localizationService, 
             IPermissionService permissionService,
             IEventPublisher eventPublisher,
-            IStoreService storeService)
+            IStoreService storeService,
+            ICustomerActivityService customerActivityService)
         {
             this._productService = productService;
             this._dateTimeHelper = dateTimeHelper;
@@ -44,6 +47,7 @@ namespace Nop.Admin.Controllers
             this._permissionService = permissionService;
             this._eventPublisher = eventPublisher;
             _storeService = storeService;
+            this._customerActivityService = customerActivityService;
         }
 
         #endregion
@@ -164,7 +168,10 @@ namespace Nop.Admin.Controllers
                 productReview.ReviewText = model.ReviewText;
                 productReview.IsApproved = model.IsApproved;
                 _productService.UpdateProduct(productReview.Product);
-                
+
+                //activity log
+                _customerActivityService.InsertActivity("EditProductRevie", _localizationService.GetResource("ActivityLog.EditProductRevie"), productReview.Id);
+
                 //update product totals
                 _productService.UpdateProductReviewTotals(productReview.Product);
 
@@ -192,6 +199,10 @@ namespace Nop.Admin.Controllers
 
             var product = productReview.Product;
             _productService.DeleteProductReview(productReview);
+
+            //activity log
+            _customerActivityService.InsertActivity("DeleteProductRevie", _localizationService.GetResource("ActivityLog.DeleteProductRevie"), productReview.Id);
+
             //update product totals
             _productService.UpdateProductReviewTotals(product);
 

paisleyadmin/veenacollections

src/Presentation/Nop.Web/Administration/Controllers/PluginController.cs
@@ -18,6 +18,7 @@ using Nop.Services.Cms;
 using Nop.Services.Common;
 using Nop.Services.Configuration;
 using Nop.Services.Localization;
+using Nop.Services.Logging;
 using Nop.Services.Payments;
 using Nop.Services.Security;
 using Nop.Services.Shipping;
@@ -46,9 +47,10 @@ namespace Nop.Admin.Controllers
         private readonly TaxSettings _taxSettings;
         private readonly ExternalAuthenticationSettings _externalAuthenticationSettings;
         private readonly WidgetSettings _widgetSettings;
-	    #endregion
+        private readonly ICustomerActivityService _customerActivityService;
+        #endregion
 
-		#region Constructors
+        #region Constructors
 
         public PluginController(IPluginFinder pluginFinder,
             IOfficialFeedManager officialFeedManager,
@@ -62,8 +64,9 @@ namespace Nop.Admin.Controllers
             ShippingSettings shippingSettings,
             TaxSettings taxSettings, 
             ExternalAuthenticationSettings externalAuthenticationSettings, 
-            WidgetSettings widgetSettings)
-		{
+            WidgetSettings widgetSettings,
+            ICustomerActivityService customerActivityService)
+        {
             this._pluginFinder = pluginFinder;
             this._officialFeedManager = officialFeedManager;
             this._localizationService = localizationService;
@@ -77,7 +80,8 @@ namespace Nop.Admin.Controllers
             this._taxSettings = taxSettings;
             this._externalAuthenticationSettings = externalAuthenticationSettings;
             this._widgetSettings = widgetSettings;
-		}
+            this._customerActivityService = customerActivityService;
+        }
 
 		#endregion 
 
@@ -300,6 +304,10 @@ namespace Nop.Admin.Controllers
 
                 //install plugin
                 pluginDescriptor.Instance().Install();
+
+                //activity log
+                _customerActivityService.InsertActivity("InstallNewPlugin", _localizationService.GetResource("ActivityLog.InstallNewPlugin"), pluginDescriptor.FriendlyName);
+
                 SuccessNotification(_localizationService.GetResource("Admin.Configuration.Plugins.Installed"));
 
                 //restart application
@@ -339,6 +347,10 @@ namespace Nop.Admin.Controllers
 
                 //uninstall plugin
                 pluginDescriptor.Instance().Uninstall();
+
+                //activity log
+                _customerActivityService.InsertActivity("UninstallPlugin", _localizationService.GetResource("ActivityLog.UninstallPlugin"), pluginDescriptor.FriendlyName);
+
                 SuccessNotification(_localizationService.GetResource("Admin.Configuration.Plugins.Uninstalled"));
 
                 //restart application
@@ -564,6 +576,9 @@ namespace Nop.Admin.Controllers
                             }
                         }
                     }
+
+                    //activity log
+                    _customerActivityService.InsertActivity("EditPlugin", _localizationService.GetResource("ActivityLog.EditPlugin"), pluginDescriptor.FriendlyName);
                 }
 
                 ViewBag.RefreshPage = true;

src/Presentation/Nop.Web/Administration/Controllers/ProductReviewController.cs
@@ -9,6 +9,7 @@ using Nop.Services.Catalog;
 using Nop.Services.Events;
 using Nop.Services.Helpers;
 using Nop.Services.Localization;
+using Nop.Services.Logging;
 using Nop.Services.Security;
 using Nop.Services.Stores;
 using Nop.Web.Framework.Controllers;
@@ -26,6 +27,7 @@ namespace Nop.Admin.Controllers
         private readonly IPermissionService _permissionService;
         private readonly IEventPublisher _eventPublisher;
         private readonly IStoreService _storeService;
+        private readonly ICustomerActivityService _customerActivityService;
 
         #endregion Fields
 
@@ -36,7 +38,8 @@ namespace Nop.Admin.Controllers
             ILocalizationService localizationService, 
             IPermissionService permissionService,
             IEventPublisher eventPublisher,
-            IStoreService storeService)
+            IStoreService storeService,
+            ICustomerActivityService customerActivityService)
         {
             this._productService = productService;
             this._dateTimeHelper = dateTimeHelper;
@@ -44,6 +47,7 @@ namespace Nop.Admin.Controllers
             this._permissionService = permissionService;
             this._eventPublisher = eventPublisher;
             _storeService = storeService;
+            this._customerActivityService = customerActivityService;
         }
 
         #endregion
@@ -164,7 +168,10 @@ namespace Nop.Admin.Controllers
                 productReview.ReviewText = model.ReviewText;
                 productReview.IsApproved = model.IsApproved;
                 _productService.UpdateProduct(productReview.Product);
-                
+
+                //activity log
+                _customerActivityService.InsertActivity("EditProductReview", _localizationService.GetResource("ActivityLog.EditProductReview"), productReview.Id);
+
                 //update product totals
                 _productService.UpdateProductReviewTotals(productReview.Product);
 
@@ -192,6 +199,10 @@ namespace Nop.Admin.Controllers
 
             var product = productReview.Product;
             _productService.DeleteProductReview(productReview);
+
+            //activity log
+            _customerActivityService.InsertActivity("DeleteProductReview", _localizationService.GetResource("ActivityLog.DeleteProductReview"), productReview.Id);
+
             //update product totals
             _productService.UpdateProductReviewTotals(product);
 