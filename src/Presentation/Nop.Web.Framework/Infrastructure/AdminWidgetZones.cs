modernWebDev9/nopCommerce

Re-designed for no-tabs solution

src/Presentation/Nop.Web.Framework/Infrastructure/AdminWidgetZones.cs
@@ -40,6 +40,7 @@
         public static string CatalogSettingsBottom => "admin_catalog_settings_bottom";
         public static string CatalogSettingsButtons => "admin_catalog_settings_buttons";
         public static string CatalogSettingsTop => "admin_catalog_settings_top";
+        public static string CategoryBlock => "admin_category_block";
         public static string CategoryDetailsButtons => "admin_category_details_buttons";
         public static string CategoryDetailsInfoBottom => "admin_category_details_info_bottom";
         public static string CategoryDetailsInfoTop => "admin_category_details_info_top";

paisleyadmin/veenacollections

src/Presentation/Nop.Web.Framework/Infrastructure/AdminWidgetZones.cs
@@ -40,22 +40,12 @@
         public static string CatalogSettingsBottom => "admin_catalog_settings_bottom";
         public static string CatalogSettingsButtons => "admin_catalog_settings_buttons";
         public static string CatalogSettingsTop => "admin_catalog_settings_top";
+        public static string CategoryDetailsBlock => "admin_category_details_block";
         public static string CategoryDetailsButtons => "admin_category_details_buttons";
-        public static string CategoryDetailsInfoBottom => "admin_category_details_info_bottom";
-        public static string CategoryDetailsInfoTop => "admin_category_details_info_top";
-        public static string CategoryDetailsProductsBottom => "admin_category_details_products_bottom";
-        public static string CategoryDetailsProductsTop => "admin_category_details_products_top";
-        public static string CategoryDetailsSeoBottom => "admin_category_details_seo_bottom";
-        public static string CategoryDetailsSeoTop => "admin_category_details_seo_top";
         public static string CategoryListButtons => "admin_category_list_buttons";
         public static string CategoryTemplateListButtons => "admin_category_template_list_buttons";
+        public static string CheckoutAttributeDetailsBlock => "admin_checkout_attribute_details_block";
         public static string CheckoutAttributeDetailsButtons => "admin_checkout_attribute_details_buttons";
-        public static string CheckoutAttributeDetailsConditionBottom => "admin_checkout_attribute_details_condition_bottom";
-        public static string CheckoutAttributeDetailsConditionTop => "admin_checkout_attribute_details_condition_top";
-        public static string CheckoutAttributeDetailsInfoBottom => "admin_checkout_attribute_details_info_bottom";
-        public static string CheckoutAttributeDetailsInfoTop => "admin_checkout_attribute_details_info_top";
-        public static string CheckoutAttributeDetailsValuesBottom => "admin_checkout_attribute_details_values_bottom";
-        public static string CheckoutAttributeDetailsValuesTop => "admin_checkout_attribute_details_values_top";
         public static string CheckoutAttributeListButtons => "admin_checkout_attribute_list_buttons";
         public static string CheckoutAttributeValueDetailsBottom => "admin_checkout_attribute_value_details_bottom";
         public static string CheckoutAttributeValueDetailsButtons => "admin_checkout_attribute_value_details_buttons";
@@ -74,7 +64,6 @@
         public static string CurrencyDetailsTop => "admin_currency_details_top";
         public static string CurrencyListButtons => "admin_currency_list_buttons";
         public static string CurrentShoppingCartListButtons => "admin_current_shopping_cart_list_buttons";
-        public static string CurrentWishlistListButtons => "admin_current_wishlist_list_buttons";
         public static string CustomerAddressDetailsBottom => "admin_customer_address_details_bottom";
         public static string CustomerAddressDetailsButtons => "admin_customer_address_details_buttons";
         public static string CustomerAddressDetailsTop => "admin_customer_address_details_top";
@@ -103,8 +92,6 @@
         public static string CustomerDetailsRewardPointsTop => "admin_customer_details_reward_points_top";
         public static string CustomerDetailsShoppingCartBottom => "admin_customer_details_shopping_cart_bottom";
         public static string CustomerDetailsShoppingCartTop => "admin_customer_details_shopping_cart_top";
-        public static string CustomerDetailsWishlistBottom => "admin_customer_details_wishlist_bottom";
-        public static string CustomerDetailsWishlistTop => "admin_customer_details_wishlist_top";
         public static string CustomerListButtons => "admin_customer_list_buttons";
         public static string CustomerReportsButtons => "admin_customer_reports_buttons";
         public static string CustomerRoleDetailsBottom => "admin_customer_role_details_bottom";
@@ -189,13 +176,8 @@
         public static string MaintenanceBottom => "admin_maintenance_bottom";
         public static string MaintenanceButtons => "admin_maintenance_buttons";
         public static string MaintenanceTop => "admin_maintenance_top";
+        public static string ManufacturerDetailsBlock => "admin_manufacturer_details_block";
         public static string ManufacturerDetailsButtons => "admin_manufacturer_details_buttons";
-        public static string ManufacturerDetailsInfoBottom => "admin_manufacturer_details_info_bottom";
-        public static string ManufacturerDetailsInfoTop => "admin_manufacturer_details_info_top";
-        public static string ManufacturerDetailsProductsBottom => "admin_manufacturer_details_products_bottom";
-        public static string ManufacturerDetailsProductsTop => "admin_manufacturer_details_products_top";
-        public static string ManufacturerDetailsSeoBottom => "admin_manufacturer_details_seo_bottom";
-        public static string ManufacturerDetailsSeoTop => "admin_manufacturer_details_seo_top";
         public static string ManufacturerListButtons => "admin_manufacturer_list_buttons";
         public static string ManufacturerTemplateListButtons => "admin_manufacturer_template_list_buttons";
         public static string MeasureListButtons => "admin_measure_list_buttons";
@@ -263,13 +245,8 @@
         public static string ProductAttributeCombinationDetailsBottom => "admin_product_attribute_combination_details_bottom";
         public static string ProductAttributeCombinationDetailsButtons => "admin_product_attribute_combination_details_buttons";
         public static string ProductAttributeCombinationDetailsTop => "admin_product_attribute_combination_details_top";
+        public static string ProductAttributeDetailsBlock => "admin_product_attribute_details_block";
         public static string ProductAttributeDetailsButtons => "admin_product_attribute_details_buttons";
-        public static string ProductAttributeDetailsInfoBottom => "admin_product_attribute_details_info_bottom";
-        public static string ProductAttributeDetailsInfoTop => "admin_product_attribute_details_info_top";
-        public static string ProductAttributeDetailsPredefinedValuesBottom => "admin_product_attribute_details_predefined_values_bottom";
-        public static string ProductAttributeDetailsPredefinedValuesTop => "admin_product_attribute_details_predefined_values_top";
-        public static string ProductAttributeDetailsUsedByProductsBottom => "admin_product_attribute_details_used_by_products_bottom";
-        public static string ProductAttributeDetailsUsedByProductsTop => "admin_product_attribute_details_used_by_products_top";
         public static string ProductAttributeListButtons => "admin_product_attribute_list_buttons";
         public static string ProductAttributeMappingDetailsButtons => "admin_product_attribute_mapping_details_buttons";
         public static string ProductAttributeMappingDetailsConditionBottom => "admin_product_attribute_mapping_details_condition_bottom";
@@ -367,13 +344,8 @@
         public static string ShoppingCartSettingsBottom => "admin_shopping_cart_settings_bottom";
         public static string ShoppingCartSettingsButtons => "admin_shopping_cart_settings_buttons";
         public static string ShoppingCartSettingsTop => "admin_shopping_cart_settings_top";
+        public static string SpecificationAttributeDetailsBlock => "admin_specification_attribute_details_block";
         public static string SpecificationAttributeDetailsButtons => "admin_specification_attribute_details_buttons";
-        public static string SpecificationAttributeDetailsInfoBottom => "admin_specification_attribute_details_info_bottom";
-        public static string SpecificationAttributeDetailsInfoTop => "admin_specification_attribute_details_info_top";
-        public static string SpecificationAttributeDetailsOptionsBottom => "admin_specification_attribute_details_options_bottom";
-        public static string SpecificationAttributeDetailsOptionsTop => "admin_specification_attribute_details_options_top";
-        public static string SpecificationAttributeDetailsUsedByProductsBottom => "admin_specification_attribute_details_used_by_products_bottom";
-        public static string SpecificationAttributeDetailsUsedByProductsTop => "admin_specification_attribute_details_used_by_products_top";
         public static string SpecificationAttributeListButtons => "admin_specification_attribute_list_buttons";
         public static string SpecificationAttributeOptionDetailsBottom => "admin_specification_attribute_option_details_bottom";
         public static string SpecificationAttributeOptionDetailsButtons => "admin_specification_attribute_option_details_buttons";