trzepak1916/nopCommerce

Added "what3words" widget

src/Presentation/Nop.Web.Framework/Infrastructure/AdminWidgetZones.cs
@@ -146,6 +146,7 @@
         public static string OrderAddressDetailsBottom => "admin_order_address_details_bottom";
         public static string OrderAddressDetailsButtons => "admin_order_address_details_buttons";
         public static string OrderAddressDetailsTop => "admin_order_address_details_top";
+        public static string OrderBillingAddressDetailsBottom => "Order_billing_address_details_bottom";
         public static string OrderDetailsBlock => "admin_order_details_block";
         public static string OrderDetailsButtons => "admin_order_details_buttons";
         public static string OrderListButtons => "admin_order_list_buttons";
@@ -154,6 +155,7 @@
         public static string OrderSettingsDetailsBlock => "admin_order_settings_details_block";
         public static string OrderSettingsReturnRequestSettingsBottom => "admin_order_settings_return_request_settings_bottom";
         public static string OrderShipmentAddButtons => "admin_order_shipment_add_buttons";
+        public static string OrderShippingAddressDetailsBottom => "OrderShippingAddressDetailsBottom";
         public static string OrderShipmentDetailsButtons => "admin_order_shipment_details_buttons";
         public static string OrderShipmentListButtons => "admin_order_shipment_list_buttons";
         public static string OrderUploadLicenseButtons => "admin_order_upload_license_buttons";

src/Presentation/Nop.Web.Framework/Infrastructure/PublicWidgetZones.cs
@@ -5,6 +5,7 @@
         public static string AccountNavigationAfter => "account_navigation_after";
         public static string AccountNavigationBefore => "account_navigation_before";
         public static string AdminHeaderLinksAfter => "admin_header_links_after";
+        public static string AddressBottom => "address_bottom";
         public static string ApplyVendorBottom => "applyvendor_bottom";
         public static string ApplyVendorTop => "applyvendor_top";
         public static string BlogListPageAfterPost => "bloglist_page_after_post";
@@ -143,10 +144,12 @@
         public static string OrderDetailsPageOverview => "orderdetails_page_overview";
         public static string OrderDetailsPageTop => "orderdetails_page_top";
         public static string OrderDetailsProductLine => "orderdetails_product_line";
+        public static string OrderSummaryBillingAddressBottom => "order_summary_billing_address_bottom";
         public static string OrderSummaryCartFooter => "order_summary_cart_footer";
         public static string OrderSummaryContentAfter => "order_summary_content_after";
         public static string OrderSummaryContentBefore => "order_summary_content_before";
         public static string OrderSummaryContentDeals => "order_summary_content_deals";
+        public static string OrderSummaryShippingAddressBottom => "order_summary_shipping_address_bottom";
         public static string OrderSummaryTotals => "order_summary_totals";
         public static string ProductBoxAddinfoAfter => "productbox_addinfo_after";
         public static string ProductBoxAddinfoBefore => "productbox_addinfo_before";

paisleyadmin/veenacollections

src/Presentation/Nop.Web.Framework/Infrastructure/AdminWidgetZones.cs
@@ -146,6 +146,7 @@
         public static string OrderAddressDetailsBottom => "admin_order_address_details_bottom";
         public static string OrderAddressDetailsButtons => "admin_order_address_details_buttons";
         public static string OrderAddressDetailsTop => "admin_order_address_details_top";
+        public static string OrderBillingAddressDetailsBottom => "order_billing_address_details_bottom";
         public static string OrderDetailsBlock => "admin_order_details_block";
         public static string OrderDetailsButtons => "admin_order_details_buttons";
         public static string OrderListButtons => "admin_order_list_buttons";
@@ -154,6 +155,7 @@
         public static string OrderSettingsDetailsBlock => "admin_order_settings_details_block";
         public static string OrderSettingsReturnRequestSettingsBottom => "admin_order_settings_return_request_settings_bottom";
         public static string OrderShipmentAddButtons => "admin_order_shipment_add_buttons";
+        public static string OrderShippingAddressDetailsBottom => "order_shipping_address_details_bottom";
         public static string OrderShipmentDetailsButtons => "admin_order_shipment_details_buttons";
         public static string OrderShipmentListButtons => "admin_order_shipment_list_buttons";
         public static string OrderUploadLicenseButtons => "admin_order_upload_license_buttons";

src/Presentation/Nop.Web.Framework/Infrastructure/PublicWidgetZones.cs
@@ -5,6 +5,7 @@
         public static string AccountNavigationAfter => "account_navigation_after";
         public static string AccountNavigationBefore => "account_navigation_before";
         public static string AdminHeaderLinksAfter => "admin_header_links_after";
+        public static string AddressBottom => "address_bottom";
         public static string ApplyVendorBottom => "applyvendor_bottom";
         public static string ApplyVendorTop => "applyvendor_top";
         public static string BlogListPageAfterPost => "bloglist_page_after_post";
@@ -137,16 +138,20 @@
         public static string OpCheckoutShippingAddressTop => "op_checkout_shipping_address_top";
         public static string OpCheckoutShippingMethodBottom => "op_checkout_shipping_method_bottom";
         public static string OpCheckoutShippingMethodTop => "op_checkout_shipping_method_top";
+        public static string OrderDetailsBillingAddress => "order_details_billing_address";
         public static string OrderDetailsPageAfterproducts => "orderdetails_page_afterproducts";
         public static string OrderDetailsPageBeforeproducts => "orderdetails_page_beforeproducts";
         public static string OrderDetailsPageBottom => "orderdetails_page_bottom";
         public static string OrderDetailsPageOverview => "orderdetails_page_overview";
         public static string OrderDetailsPageTop => "orderdetails_page_top";
         public static string OrderDetailsProductLine => "orderdetails_product_line";
+        public static string OrderDetailsShippingAddress => "order_details_shipping_address";
+        public static string OrderSummaryBillingAddress => "order_summary_billing_address";
         public static string OrderSummaryCartFooter => "order_summary_cart_footer";
         public static string OrderSummaryContentAfter => "order_summary_content_after";
         public static string OrderSummaryContentBefore => "order_summary_content_before";
         public static string OrderSummaryContentDeals => "order_summary_content_deals";
+        public static string OrderSummaryShippingAddress => "order_summary_shipping_address";
         public static string OrderSummaryTotals => "order_summary_totals";
         public static string ProductBoxAddinfoAfter => "productbox_addinfo_after";
         public static string ProductBoxAddinfoBefore => "productbox_addinfo_before";